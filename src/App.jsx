39beab30dd8f9dafb317690902fdf9e815d73d39
Shubham29lko@gmail.com <162909471+ShubhamTiwari29@users.noreply.github.com>
2024-04-22 20:41:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+
+
+  return (
+    <>
+   <div class="container text-center">
+  <div class="row">
+    <div class="col-6">
+      <input type="text" placeholder='Enter Your Todo' /> 
+    </div>
+    <div class="col-4">
+      <input type="date" name="" id="" />
+    </div>
+    <div class="col-2"><input type="submit" value="Add" /></div>
+  </div>
+  
+</div>
+    </>
+  )
+}
+
+export default App
